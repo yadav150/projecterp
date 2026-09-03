2a82ea924abcf396dd36ddbaeaf911a508eca560
Yadav Subba <yadavsubba2003@gmail.com>
2026-09-03 17:38:23 +0530

    Update and rename firebase.js to firebase-config.js

---
+++ js/firebase-config.js
@@ -0,0 +1,22 @@
+// js/firebase-config.js
+(function() {
+  'use strict';
+
+  var firebaseConfig = {
+    apiKey: "AIzaSyB4GChSVJaGJOfrgFAtY8qd2TEZR1roA9U",
+    authDomain: "janakiprofessionalacademy.firebaseapp.com",
+    projectId: "janakiprofessionalacademy",
+    storageBucket: "janakiprofessionalacademy.firebasestorage.app",
+    messagingSenderId: "251032601039",
+    appId: "1:251032601039:web:4c6829d2591458f45a12ba",
+    measurementId: "G-707JTX1XKM"
+  };
+
+  if (typeof firebase !== 'undefined' && !firebase.apps.length) {
+    firebase.initializeApp(firebaseConfig);
+  }
+
+  // Expose services
+  window.db = firebase.firestore ? firebase.firestore() : null;
+  window.auth = firebase.auth ? firebase.auth() : null;
+})();
